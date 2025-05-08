ad8e1d75e52d9c8e9956876facc09f6f5681d8fc
suhanov.egor5 <suhanov.egor5@wildberries.work>
2025-05-08 17:24:56 +0300

    feat: MultiError

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +13,67 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errors []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	const sep = "\t* "
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprintf("%d errors occured:\n", len(e.errors)))
+
+	for _, err := range e.errors {
+		sb.WriteString(sep)
+		sb.WriteString(err.Error())
+	}
+
+	sb.WriteRune('\n')
+
+	return sb.String()
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	notNilErrors := 0
+
+	if err != nil {
+		notNilErrors++
+	}
+
+	for _, err := range errs {
+		if err != nil {
+			notNilErrors++
+		}
+	}
+
+	if notNilErrors == 0 {
+		return nil
+	}
+
+	me, ok := err.(*MultiError)
+	if !ok {
+		me = &MultiError{
+			errors: make([]error, 0, notNilErrors),
+		}
+
+		if err != nil {
+			me.errors = append(me.errors, err)
+		}
+	} else {
+		notNilErrors-- // Так как мы подсчитали me
+		me.errors = slices.Grow(me.errors, notNilErrors)
+	}
+
+	for _, err := range errs {
+		if err != nil {
+			me.errors = append(me.errors, errs...)
+		}
+	}
+
+	return me
+}
+
+func (e *MultiError) Unwrap() []error {
+	return e.errors
 }
 
 func TestMultiError(t *testing.T) {
